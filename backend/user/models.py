182c7d9d2fecc0a47639d6fec1fa91c6d4d615e7
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-07-09 18:20:49 -0300

    Initiate Customer and Employee Models

--- backend/user/models.py
+++ backend/user/models.py
@@ -15,6 +15,18 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.is_costumer = True
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_employee(self, email, password=None, **extra_fields):
+        """Create, save and return a new employee."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.is_employee = True
         user.set_password(password)
         user.save()
 
@@ -38,6 +50,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=30, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+    is_employee = models.BooleanField(default=False)
 
     objects = UserManager()
 
@@ -51,3 +65,51 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    cpf = models.CharField(max_length=11)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.email
+
+
+class Permission(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    department = models.CharField(max_length=50)
+    permissions = models.ManyToManyField(Permission)
+
+    def __str__(self):
+        return self.user.email
+
+
+class Address(models.Model):
+    customer = models.ForeignKey(
+        Customer, related_name='addresses', on_delete=models.CASCADE)
+    street = models.CharField(max_length=100)
+    number = models.CharField(max_length=10)
+    neighborhood = models.CharField(max_length=100)
+    complement = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.street}, \
+                 {self.number}, \
+                 {self.city}, \
+                 {self.state}, \
+                 {self.zip_code}"
